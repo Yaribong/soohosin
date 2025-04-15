1694c0bdbf3ca9899eccb8f9dba332f71749fa47
SooHoSin <yaribong@SooHoSin.local>
2025-04-15 17:38:35 +0900

    fix: 사이트맵 baseUrl 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://soohosin.vercel.app';
+  const baseUrl = 'https://soohosin.com';
   
   return [
     {
